079da7878eceae603d6bd6b6da1625ebf431bc8e
Jung Nam <j.nam0808@gmail.com>
2021-07-24 00:43:54 -0400

    started adding functionality on javascript file

---
+++ main.js
@@ -0,0 +1,28 @@
+`use strict`;
+
+// This code makes navbar transparent when it is on top with the background and changes color.
+const navbar = document.querySelector(`#navbar`);
+const navbarHeight = navbar.getBoundingClientRect().height;
+
+document.addEventListener("scroll", () => {
+  //   console.log(window.scrollY);
+  //   console.log(`navbarHeight: ${navbarHeight}`);
+  if (window.scrollY > navbarHeight) {
+    navbar.classList.add("navbar-dark");
+  } else {
+    navbar.classList.remove("navbar-dark");
+  }
+});
+
+// Handles scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener("click", (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+  console.log(event.target.dataset.link);
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
+});
